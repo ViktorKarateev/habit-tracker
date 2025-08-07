252b57ff230554bcf5bc385aeb6841ad50d0838a
karat <karateev.92@gmail.com>
2025-08-07 23:10:45 +0400

    реализован CRUD для модели Habit через ViewSet

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Habit
+from .serializers import HabitSerializer
 
-# Create your views here.
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
