e27af2a988a19dac910196bf6ff00e86440e58f8
karat <karateev.92@gmail.com>
2025-08-10 22:19:59 +0400

    добавлен TELEGRAM_BOT_TOKEN

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -4,6 +4,10 @@ from celery.schedules import crontab
 from dotenv import load_dotenv
 
 
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
