e9d6d3f90f8bdf0c0d61cdcfdd50aa32d8d08659
karat <karateev.92@gmail.com>
2025-08-11 23:22:30 +0400

    проверка флейк8

--- habits/admin.py
+++ habits/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Habit
 
+
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
     list_display = ("user", "action", "time", "is_pleasant", "is_public")
