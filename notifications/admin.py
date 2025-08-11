57130fabc9de15800a5fa7fbd198485511120042
karat <karateev.92@gmail.com>
2025-08-11 23:32:39 +0400

    фикс

--- notifications/admin.py
+++ notifications/admin.py
@@ -4,5 +4,6 @@ from .models import TelegramAccount
 
 @admin.register(TelegramAccount)
 class TelegramAccountAdmin(admin.ModelAdmin):
-    list_display = ("user", "chat_id", "created_at")
+    list_display = ("id", "user", "chat_id")  # убрали created_at
     search_fields = ("user__username", "chat_id")
+    list_select_related = ("user",)
